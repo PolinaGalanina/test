f2e7c2895c688debb7eb812f37fb3c192faf0d86
PolinaGalanina <galanina_pa@mail.ru>
2024-04-18 19:08:12 +0300

    базовая версия окон

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(12-1)
+from PyQt5.QtWidgets import QApplication
+from first import First
+from second import Second
+
+app = QApplication([])
+win1 = First()
+win2 = Second()
+app.exec()
---
+++ second.py
@@ -0,0 +1,10 @@
+#тут будет класс с окном - вводом информации
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+class Second():
+    def __init__(self):
+        self.win = QWidget()
+        self.win.show()
+        
+
+
